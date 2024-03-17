3aa76c1163227c7e2199da6a1bd8d16b2826664f
marian-glt <38663778+marian-glt@users.noreply.github.com>
2024-03-17 16:24:48 +0000

    wrote section 0 and created build file

---
+++ src/getting-started.h
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int printHelloWorld() {
+	std::cout << "Hello, world!" << std::endl;
+
+	return 0;
+}
+
+int printSum(int a, int b) {
+	std::cout << a + b << std::endl;
+
+	return 0;
+}
+
+int printSpeechMark() {
+	std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;
+
+	return 0;
+}
+
+int printTab() {
+	std::cout << "Score:\t 60/100" << std::endl;
+
+	return 0;
+}
+
+int shortestProgramViable() {
+	return 0;
+}
+
+int printNewLineHelloWorld() {
+	std::cout << "Hello,\nWorld!" << std::endl;
+
+	return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <getting-started.h>
+#include <array>
+
+static int executeGettingStartedPrograms() {
+	printHelloWorld();
+
+	printSum(10, 7);
+
+	printSpeechMark();
+
+	printTab();
+
+	shortestProgramViable();
+
+	printNewLineHelloWorld();
+
+	return 0;
+}
+
+int main() {
+	
+	executeGettingStartedPrograms();
+
+	return 0;
+}
